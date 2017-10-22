95e0c1bf9a6768f9272236f9ab38fa66236d73e9
huangdarz <31122258+huangdarz@users.noreply.github.com>
2017-10-22 16:28:47 +1100

    Add files via upload

---
+++ Pokeball.py
@@ -0,0 +1,196 @@
+from microbit import *
+import random
+import radio
+
+radio.on()
+radio.config(channel=16)
+radio.config(queue=2)
+
+#List of pokemon
+pokemon = ["Pikachu", "Squirtle", "Eevee", "Bulbasaur", "Pidgey", "Dragonite", "Charizard", "Unown", "Mewtwo", "Kylo Ren"]
+pokedex = {}
+
+#Loading Images
+POKEBALL = Image('09990:90009:99399:90009:09990:')
+INVERTED = POKEBALL.invert()
+
+start_up = 0
+start_time = 800
+fled = 0
+
+
+#Function for Pokemon appearances
+def pkm_appearance(a_chance1, a_chance2, a_chance3, a_chance4, pkm_start1, pkm_end1, pkm_start2, pkm_end2):
+    global start_up
+    global msg
+    appearance_chance = random.randrange(21) 
+    if appearance_chance >= a_chance1 and appearance_chance <= a_chance2:
+        pokemon_name = int (random.randrange(pkm_start1, pkm_end1))
+        radio.send(pokemon[pokemon_name]) 
+        start_up = 3
+    elif appearance_chance >= a_chance3 and appearance_chance <= a_chance4 :
+        pokemon_name = random.randrange(pkm_start2, pkm_end2)
+        radio.send(pokemon[pokemon_name])
+        start_up = 3
+    else:
+        wait_time = random.randrange(3000)
+        sleep(wait_time)
+
+#Function for Pokemon Catch Rate
+def catch_rate(message, rate_start, rate_end):
+    global start_up
+    global fled
+    catch_chance = random.randrange(101)
+    if catch_chance >= rate_start and catch_chance <= rate_end:
+        radio.send(message)
+        fled = 1
+    else:
+        fled = 2
+        sleep(2000)
+
+#Function for adding to Dictionary Pokedex        
+def dict_add(pkm, lvl):
+    pokedex[pkm] = lvl
+
+#Function to add Pokemon to Pokedex
+def pokedex_update():
+    global start_up
+    while start_up == 4:
+        pokemon_caught = radio.receive()
+        parts = pokemon_caught.split(':')
+        pkm = parts[0]
+        lvl = parts[-1]
+        if pokemon_caught:
+            dict_add(pkm, lvl)
+            radio.send('updated')
+            sleep(200)
+            start_up = 2
+
+#Function for Shake action to catch            
+def catch_action(message, rate1, rate2):
+    global start_up
+    global fled
+    global msg
+    while start_up == 3: 
+        if accelerometer.was_gesture('shake'):
+            catch_rate(message, rate1, rate2)
+            if fled == 1:
+                radio.send(msg)
+                start_up = 4
+                fled = 0
+            if fled == 2:
+                start_up  = 2
+                fled = 0
+            msg = None
+#-----------------START OF PROGRAM---------------------------------
+        
+while True:
+    #Start Screen
+    while start_up == 0:
+        if button_a.was_pressed():
+            radio.send("Press A Button")
+            start_up = 1
+
+    #Loading Screen
+    while start_up == 1:
+        display.clear()
+        if button_a.was_pressed():
+            radio.send("clear")
+            while start_time > 0:
+                display.show(POKEBALL)
+                sleep(start_time)
+                display.show(INVERTED)
+                sleep(start_time)
+                start_time -= 50
+        if start_time == 0:
+            start_up = 2
+            display.clear()
+            radio.send("msg")
+    
+    #Compass Calibration
+    if compass.is_calibrated:
+        cmps = compass.heading()
+    else:
+        compass.calibrate()
+        cmps = compass.heading()
+
+    #Pokemon Zones
+    while start_up == 2:
+        display.scroll('2')
+        sleep(3000)
+        cmps = compass.heading()
+        if cmps > 0 and cmps <= 90:
+            pkm_appearance(0, 10, 18, 20, 0, 3, 7, 9)
+            continue
+        if cmps > 90 and cmps <= 180:
+            pkm_appearance(0, 10, 15, 19, 0, 2, 5, 8)
+            continue
+        if cmps > 180 and cmps <= 270:
+            pkm_appearance(0, 5, 10, 18, 0, 3, 3, 6)
+            continue
+        if cmps > 270 and cmps <= 0:
+            pkm_appearance(0, 15, 18, 20, 0, 5, 9, 10)
+            continue
+    
+    #Pokemon Catch Rate
+    while start_up == 3:
+        display.scroll('3')
+        msg = radio.receive()
+        if msg:
+            if msg == pokemon[0]:
+                catch_action(msg, 20, 100)
+                msg = None
+                continue
+            elif msg == pokemon[1]:
+                catch_action(msg, 20, 100)
+                msg = None
+                continue
+            elif msg == pokemon[2]:
+                catch_action(msg, 20, 100)
+                msg = None
+                continue
+            elif msg == pokemon[3]:
+                catch_action(msg, 20, 100)
+                msg = None
+                continue
+            elif msg == pokemon[4]:
+                catch_action(msg, 25, 100)
+                msg = None
+                continue
+            elif msg == pokemon[5]:
+                catch_action(msg, 50, 100)
+                msg = None
+                continue
+            elif msg == pokemon[6]:
+                catch_action(msg, 50, 100)
+                msg = None
+                continue
+            elif msg == pokemon[7]:
+                catch_action(msg, 75, 100)
+                msg = None
+                continue
+            elif msg == pokemon[8]:
+                catch_action(msg, 75, 100)
+                msg = None
+                continue
+            elif msg == pokemon[9]:
+                catch_action(msg, 85, 100)
+                msg = None
+                continue
+
+    #Pokedex Entry
+    while start_up == 4:
+        msg = ""
+        display.scroll('4')
+        sleep(2000)
+        pokedex_update()
+        sleep(400)
+        start_up = 2
+
+
+
+
+    
+        
+        
+    
---
+++ Pokedex.py
@@ -0,0 +1,207 @@
+from microbit import *
+import radio
+import random
+import music
+
+#------------------LCD FUNCTIONS----------------------------------
+
+# pin connections
+rs = pin0
+enable = pin1
+datapins = [pin8, pin12, pin2, pin13]
+
+# commands
+LCD_CLEARDISPLAY = 0x01
+LCD_RETURNHOME = 0x02
+LCD_ENTRYMODESET = 0x04
+LCD_DISPLAYCONTROL = 0x08
+LCD_CURSORSHIFT = 0x10
+LCD_FUNCTIONSET = 0x20
+LCD_SETCGRAMADDR = 0x40
+LCD_SETDDRAMADDR = 0x80
+
+# flags for display entry mode
+LCD_ENTRYRIGHT = 0x00
+LCD_ENTRYLEFT = 0x02
+LCD_ENTRYSHIFTINCREMENT = 0x01
+LCD_ENTRYSHIFTDECREMENT = 0x00
+
+# flags for display on/off control
+LCD_DISPLAYON = 0x04
+LCD_DISPLAYOFF = 0x00
+LCD_CURSORON = 0x02
+LCD_CURSOROFF = 0x00
+LCD_BLINKON = 0x01
+LCD_BLINKOFF = 0x00
+
+# flags for display/cursor shift
+LCD_DISPLAYMOVE = 0x08
+LCD_CURSORMOVE = 0x00
+LCD_MOVERIGHT = 0x04
+LCD_MOVELEFT = 0x00
+
+# flags for function set
+LCD_8BITMODE = 0x10
+LCD_4BITMODE = 0x00
+LCD_2LINE = 0x08
+LCD_1LINE = 0x00
+LCD_5x10DOTS = 0x04
+LCD_5x8DOTS = 0x00
+
+
+def InitDisplay():
+    # at least 50ms after power on
+    sleep(50)
+    # send rs, enable low - rw is tied to GND
+    rs.write_digital(0)
+    enable.write_digital(0)
+    write4bits(0x03)
+    sleep(5)
+    write4bits(0x03)
+    sleep(5)
+    write4bits(0x03)
+    sleep(2)
+    write4bits(0x02)
+    send(LCD_FUNCTIONSET | 0x08, 0)
+    sleep(5)
+    send(LCD_FUNCTIONSET | 0x08, 0)
+    sleep(2)
+    send(LCD_FUNCTIONSET | 0x08, 0)
+    sleep(2)
+    send(LCD_FUNCTIONSET | 0x08, 0)
+    sleep(2)
+    send(LCD_DISPLAYCONTROL | LCD_DISPLAYON | LCD_CURSOROFF | LCD_BLINKOFF,0)
+    clear()
+    send(LCD_ENTRYMODESET | LCD_ENTRYLEFT | LCD_ENTRYSHIFTDECREMENT,0)
+     
+# high level commands    
+def clear():
+    send(LCD_CLEARDISPLAY,0)
+    sleep(2)
+
+def home():
+    send(LCD_RETURNHOME,0)
+    sleep(2)
+
+def setCursor(col, row):
+    orpart = col
+    if row>0:
+        orpart = orpart + 0x40
+    send(LCD_SETDDRAMADDR | orpart, 0)
+
+def showText(t):
+    for c in t:
+        send(ord(c), 1)
+
+# mid and low level commands        
+def send(value, mode):
+    rs.write_digital(mode)
+    write4bits(value>>4)
+    write4bits(value)
+    
+def pulseEnable():
+    enable.write_digital(0)
+    sleep(1)
+    enable.write_digital(1)
+    sleep(1)
+    enable.write_digital(0)
+    sleep(1)
+
+def write4bits(value):
+    for i in range(0,4):
+        datapins[i].write_digital((value>>i) & 0x01)
+    pulseEnable()
+
+#----------------END OF LCD FUNCTIONS------------------------------------------------------------------    
+    
+InitDisplay()
+radio.on()
+radio.config(channel=16)
+radio.config(queue=2)
+start_up = 0
+
+#Start Screen
+while True:
+    msg = radio.receive()
+    if msg == 'msg':
+        clear()
+        break
+    elif msg:
+        while start_up == 0:
+            showText(msg)
+            setCursor(0, 1)
+            showText("to Start")
+            start_up += 1
+        while start_up == 1:
+            msg = radio.receive()
+            if msg:
+                clear()
+                showText("Loading...")
+                start_up += 1
+
+clear()
+while True:
+    #Finding Pokemon Section
+    while start_up == 2:
+        sleep(2000)
+        msg = radio.receive()
+        pkm = msg
+        if msg:
+            clear()
+            showText("A wild ")
+            showText(msg)
+            setCursor(0, 1)
+            showText("has appeared!")
+            sleep(4000)
+            start_up = 3
+        else:
+            clear()
+            showText('Finding')
+            setCursor(0,1)
+            showText('Pokemon...')
+            sleep(500)
+            continue
+
+    #Pokemon level and "Shake to Catch"
+    while start_up == 3:
+        radio.send(msg)
+        clear()
+        display.clear()
+        lvl = str(random.randrange(2, 8))
+        showText(msg + ':' + lvl)
+        pkdx = msg + ':' + lvl
+        setCursor(0, 1)
+        showText('Shake to catch')
+        radio.send(msg)
+        start_up = 4
+    
+    #Pokemon Caught Status and Pokedex Update
+    while start_up == 4:
+        catch_succesful = radio.receive()
+        if catch_succesful == msg:
+            clear()
+            showText(msg)
+            setCursor(0, 1)
+            showText('has been caught!')
+            sleep(2000)
+            radio.send(pkdx)
+            catch_succesful = ""
+            start_up += 1
+            while start_up == 5:
+                update = radio.receive()
+                if update == 'updated':
+                    clear()
+                    showText('Your Pokedex has')
+                    setCursor(0, 1)
+                    showText('been updated')
+                    msg = None
+                    start_up = 2
+                    continue
+        if catch_succesful == 'fled':
+            clear()
+            showText(msg)
+            setCursor(0, 1)
+            showText('has fled!')
+            sleep(1500)
+            start_up = 2 
+            continue
